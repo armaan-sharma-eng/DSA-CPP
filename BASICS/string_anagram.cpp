c9fb2729b1fcbc3a5a06f14680e789ba6b225c30
armaan-sharma-eng <armaan.sharma.eng@gmail.com>
2025-12-24 21:47:47 +0530

    general string anagram

--- BASICS/string_anagram.cpp
+++ BASICS/string_anagram.cpp
@@ -5,6 +5,7 @@ class anagram
     {
         string str1,str2;
         int len1,len2,i,j,count=0;
+        int *b1,*f1;
         public:
 
         anagram(string s1,string s2)
@@ -13,31 +14,77 @@ class anagram
                 str2=s2;
             }
         
-        void length()
+        int length()
             {
                 for(len1=0;str1[len1]!='\0';len1++);
                 for(len2=0;str2[len2]!='\0';len2++);
-                cout<<len1<<"\t"<<len2<<endl;
+                //cout<<len1<<"\t"<<len2<<endl;
+                int b[len1],f[len1];
+                int x=find_frequency(b,f);
+                return x;                
+            }
+
+        int find_frequency(int b[],int f[])
+            {
+                for(i=0;i<len1;i++)
+                {
+                    b[i]=-1;
+                }
+
+                for(i=0;i<len1;i++)
+                {
+                    count=1;
+                    if(b[i]==0)
+                    continue;
+
+                    for(j=i+1;j<len1;j++)
+                    {
+                        if(b[i]==-1)
+                        {
+                            if(str1[i]==str1[j])
+                                {
+                                    count++;
+                                    b[j]=0;
+                                }
+                        }        
+                    }
+                    f[i]=count;
+                }
+
+                for(i=0;i<len1;i++)
+                    {
+                        if(b[i]==-1)
+                        //cout<<str1[i]<<"  "<<f[i]<<endl;
+                    }
+
+                int x=check(b,f);  
+                return x;  
             }    
 
-        bool check()
+        bool check(int b[],int f[])
             {
                 if(len1!=len2)
                 return false;
 
-                for(i=0;i<len1;i++)
+                for(i=0;i<len2;i++)
                     {
-                        for(j=0;j<=len2;j++)
-                            {
-                                if(str1[i]==str2[j])
-                                    count++;
-                            }
-                        if(count!=i+1)
-                        return false;    
+                        count=0;
+                        if(b[i]==-1)
+                        {
+
+                            for(j=0;j<=len2;j++)
+                                {
+                                        if(str1[i]==str2[j])
+                                        count++;
+                                }     
+                            if(count!=f[i])
+                            return false;   
+                        } 
                     }
                 return true;    
             }    
     };
+
 int main()
     {
         string s1,s2;
@@ -47,8 +94,9 @@ int main()
         cin>>s2;
         
         class anagram obj(s1,s2);
-        obj.length();
-        if(obj.check())
+        int x=obj.length();
+        cout<<endl<<endl<<x<<endl;
+        if(x)
             {
                 cout<<"yes this string is anagram\n";
             }
