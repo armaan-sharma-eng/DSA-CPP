e617592c89fc57e936ec562bdc3bb0154f92a3bb
Armaan Sharma <armaan.sharma.eng@gmail.com>
2025-12-27 22:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (15.20%) - LeetSync

---
+++ LEETCODE/2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        vector<int> ary;
+        for(int i=0;i<nums.size();i++)
+            {
+                ary.push_back(nums[i]);
+            }
+        for(int i=0;i<nums.size();i++)
+            {
+                ary.push_back(nums[i]);
+            }
+        return ary;    
+    }
+};
