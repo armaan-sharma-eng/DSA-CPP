9b9903d51b1a6ed48ecc83223bb8b7ecd7b065ef
Armaan Sharma <armaan.sharma.eng@gmail.com>
2025-12-28 15:29:29 +0530

    Time: 34 ms (36.80%) | Memory: 13.9 MB (93.28%) - LeetSync

--- LEETCODE/1-two-sum/two-sum.cpp
+++ LEETCODE/1-two-sum/two-sum.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        int i;
-        int j;
+        int i,j;
         for(i=0;i<nums.size();i++)
             {
                 auto ptr=find(nums.begin()+(i+1),nums.end(),target-nums[i]);
