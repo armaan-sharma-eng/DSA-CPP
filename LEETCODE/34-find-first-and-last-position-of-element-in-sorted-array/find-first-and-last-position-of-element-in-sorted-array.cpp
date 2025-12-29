fb1569d9dbcab1b1fa6d710501fa0a1dd65621cf
Armaan Sharma <armaan.sharma.eng@gmail.com>
2025-12-30 00:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (85.47%) - LeetSync

--- LEETCODE/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ LEETCODE/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,32 +2,55 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) 
     {
-        int fi=-1,li=-1;
-        int i;
-        for(i=0;i<nums.size();i++)
+        int begin=0,end=nums.size()-1,mid,pos=-1;
+        while(begin<=end)
             {
-                if(target==nums[i])
-                break;
+                mid=begin-(begin-end)/2;
+                if(target==nums[mid])
+                    {
+                        pos=mid;
+                        break;
+                    }
+                else if(nums[mid]>target)
+                    {
+                        end=mid-1;
+                    }
+                else
+                    {
+                        begin=mid+1;
+                    }
             }
-        if(i==nums.size())
+        int fi=-1,li=-1;
+        if(pos==-1)
             {
                 nums.clear();
                 nums.push_back(fi);
                 nums.push_back(li);
                 return nums;
             }
-        fi=i;    
-        int j;
-        for(j=i;j<nums.size();j++)
+        
+        
+        for(int i=pos;i>=0;i--)
             {
-                if(nums[j]!=nums[i])
-                break;
+                fi=i;
+                if(nums[pos]!=nums[i])
+                {
+                    fi=i+1;
+                    break;
+                }
             }
-        li=j-1;
+        for(int i=pos;i<nums.size();i++)
+            {
+                li=i;
+                if(nums[pos]!=nums[i])
+                {
+                    li=i-1;
+                    break;
+                }
+            }
         nums.clear();
         nums.push_back(fi);
         nums.push_back(li);
         return nums;
-        
     }
 };
