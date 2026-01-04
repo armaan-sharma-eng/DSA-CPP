3ee69066e2f938dd75d04905464ad8c8894768f7
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-01-04 12:07:41 +0530

    Time: 25 ms (5.27%) | Memory: 24.4 MB (5.20%) - LeetSync

---
+++ LEETCODE/136-single-number/single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        map<int,int> mp;
+
+        for(auto val:nums)
+            mp[val]++;
+
+        for(int i=0;i<nums.size();i++)
+            {
+                if(mp[nums[i]]==1)
+                return nums[i];
+            }      
+        return -1;  
+    }
+};
