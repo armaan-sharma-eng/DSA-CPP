15fda3df6d40c97c458029f50dbb0fa4f32b9867
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-01-04 21:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.35%) - LeetSync

--- LEETCODE/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ LEETCODE/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,38 +2,16 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) 
     {
-        auto mn=min_element(nums.begin(),nums.end());
-        auto midx=mn-nums.begin();
-        int k=midx;
-
-        sort(nums.begin(),nums.end());
-
-        int begin=0,end=nums.size()-1;
-        int mid,pos=-1;
-        while(begin<=end)
+        int pos=-1;
+        for(int i=0;i<nums.size();i++)
             {
-                mid=begin-(begin-end)/2;
-                if(nums[mid]==target)
+                if(target==nums[i])
                     {
-                        pos=mid;
+                        pos=i;
                         break;
                     }
-                else if(nums[mid]<target)
-                    {
-                        begin=mid+1;
-                    }
-                else
-                    {
-                        end=mid-1;
-                    }
             }
-            if(pos==-1)
-                return -1;
 
-            if(k+pos<nums.size())
-                return pos+k;
-
-            else
-                return pos+k-nums.size();
+        return pos;
     }
 };
