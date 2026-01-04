c6d2fc1e12792d0f6de37353e3d1e1c9763ea846
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-01-04 12:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (25.29%) - LeetSync

--- LEETCODE/136-single-number/single-number.cpp
+++ LEETCODE/136-single-number/single-number.cpp
@@ -2,16 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
-        map<int,int> mp;
-
+        int ans=0;
         for(auto val:nums)
-            mp[val]++;
+            ans=ans^val;
 
-        for(int i=0;i<nums.size();i++)
-            {
-                if(mp[nums[i]]==1)
-                return nums[i];
-            }      
-        return -1;  
+        return ans;
     }
 };
