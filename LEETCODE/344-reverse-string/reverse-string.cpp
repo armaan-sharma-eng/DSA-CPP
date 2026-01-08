bd3ad81b075113a3f2473d66dde09188280f8074
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-01-08 12:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (79.31%) - LeetSync

--- LEETCODE/344-reverse-string/reverse-string.cpp
+++ LEETCODE/344-reverse-string/reverse-string.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     void reverseString(vector<char>& s) 
     {
-        int n=s.size();
-        for(int i=0;i<n/2;i++)
+        for(int i=0;i<s.size()/2;i++)
             swap(s[i],s[s.size()-1-i]);
     }
 };
