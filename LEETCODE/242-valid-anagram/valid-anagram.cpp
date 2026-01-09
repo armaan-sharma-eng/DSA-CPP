72d0b8df2b9b0fb60b8f5f6e77ae166e78ef3404
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-01-09 20:23:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.76%) - LeetSync

---
+++ LEETCODE/242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+        {
+            if(s.length()!=t.length())
+            return false;
+            
+            int as[256]={0};
+            for(int i=0;i<s.length();i++)
+                {
+                    as[s[i]]++;
+                    as[t[i]]--;
+                }
+            for(int i=0;i<256;i++)
+                {
+                    if(as[i]!=0)
+                    return false;
+                }
+            return true;    
+        }
+};  
