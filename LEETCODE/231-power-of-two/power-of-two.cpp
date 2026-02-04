2f21187d3e030a84c7ec3bb67173f916ffbad2cf
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-02-04 13:30:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (98.41%) - LeetSync

---
+++ LEETCODE/231-power-of-two/power-of-two.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        if(n<=0)
+            return false;
+        if(n==1||n==2)
+            return true;
+        while(n!=0)
+        {
+            if(n%2==1)
+                return false;
+            n=n/2;
+            if(n==2)
+                return true;
+        }
+        return true;
+    }
+};
