52e30097188f34da88dcde7e6800da6107533909
Armaan Sharma <armaan.sharma.eng@gmail.com>
2026-03-26 14:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (53.09%) - LeetSync

---
+++ LEETCODE/367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) 
+    {
+        if(num==1)
+            return true;
+        for(long i=0;i*i<=num;i++)
+        {
+            if(i*i==num)
+                return true;
+        }
+        return false;        
+    }
+};
